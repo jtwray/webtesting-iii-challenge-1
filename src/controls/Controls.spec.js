67f742cf71c1ed4a26dbd0901d3668f168e4fc69
tucker <tuckerwray84@gmail.com>
2019-12-06 21:35:15 -0500

    practices writing test for user actions rather than ui implentation

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,41 @@
-// Test away!
+import React from 'react'
+import { render, cleanup, fireEvent, waitForElement } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+import Controls from './Controls'
+
+afterEach(cleanup);
+
+describe('Controls Operate as expected', () => {
+
+    test(`renders correctly`, () => {
+        const controls = render(<Controls />);
+        expect(controls).toMatchSnapshot();
+    })
+
+    test(`Door operates correctly on user interaction`, () => {
+        const controls = render(<Controls locked={false} closed={false} />)
+        const closeGate = controls.getByText(/close gate/i)
+        fireEvent.click(closeGate)
+        expect(controls.getByText(/lock gate/i)).toBeEnabled()
+        expect(controls.getByText(/open gate/i)).toBeEnabled()
+    })
+    test(`Locking gate enables unlock control`, () => {
+        const controls = render(<Controls locked={false} closed={true} />)
+      
+        const lockGate = controls.getByText(/lock gate/i)
+        fireEvent.click(lockGate)
+        expect(controls.getByText(/unlock gate/i)).toBeEnabled()
+    })
+    test(`Open gate enables after unlock`, () => {
+        const controls = render(<Controls locked={true} closed={true} />)
+        const unlockGate = controls.getByText(/unlock gate/i)
+        fireEvent.click(unlockGate)
+        expect(controls.getByText(/open gate/i)).toBeEnabled()
+    })
+    test(`LockGate disables after opening`, () => {
+        const controls = render(<Controls locked={false} closed={true} />)
+        const openGate = controls.getByText(/open gate/i)
+        fireEvent.click(openGate)
+        expect(controls.getByText(/lock gate/i)).not.toBeEnabled()
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,31 @@
 // Test away!
+
+
+import React from 'react'
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Display from './Display'
+
+describe('Display',()=>{
+    test('renders correctly',()=>{
+        const display =render(<Display/>);
+        expect(display).toMatchSnapshot();
+    })
+test('Does not notify user "Open" if locked is true',()=>{
+    const display=render(<Display locked={true}/>)
+    const lockGate = new RegExp(`open`,`i`);
+    expect(lockGate).not.toBeEnabled()
+   
+})
+test('displays green if not locked',()=>{
+    const display=render(<Display locked={true}/>)
+    const openClass = display.getByText(`openClass`)
+})
+test('Does not notify user "Open" if locked is true',()=>{
+    const display=render(<Display locked={true}/>)
+    const openAnyCase = new RegExp(`open`,`i`);
+    expect(display).not.toBe(openAnyCase)
+   
+})
+})
